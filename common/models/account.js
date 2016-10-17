95eb5a75e86dc972c06b04edc3011b40921e9613
System Administrator <root@Desarrollos-Mac-mini.local>
2016-10-17 13:46:56 -0600

    configurations to send email

--- common/models/account.js
+++ common/models/account.js
@@ -2,4 +2,33 @@
 
 module.exports = function(Account) {
 
+	//send verification email after registration
+  Account.afterRemote('create', function(context, userInstance, next) {
+    console.log('> Account.afterRemote triggered');
+
+    var options = {
+      type: 'email',
+      to: userInstance.email,
+      from: 'noreply@loopback.com',
+      subject: 'Thanks for registering.',
+      template: path.resolve("/", '../../server/views/verify.ejs'),
+      redirect: '/verified',
+      user: Account
+    };
+
+    userInstance.verify(options, function(err, response, next) {
+      if (err) return next(err);
+
+      console.log('> verification email sent:', response);
+
+      context.res.render('response', {
+        title: 'Signed up successfully',
+        content: 'Please check your email and click on the verification link ' -
+            'before logging in.',
+        redirectTo: '/',
+        redirectToLinkText: 'Log in'
+      });
+    });
+  });
+
 };
