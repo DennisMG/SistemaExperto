7f3149dc918af76abcd5798b434a8216d2b1af69
Dennis Molina <dennismolina.17@gmail.com>
2016-12-05 21:47:16 -0600

    added api URL

--- common/models/account.js
+++ common/models/account.js
@@ -22,7 +22,7 @@ module.exports = function(Account) {
       subject: 'Thanks for registering.',
       template: path.resolve(__dirname,'../../server/views/verify.ejs'), 
       redirect: '/verified',
-      verifyHref: 'http://localhost:3000/api/Accounts/confirm?uid='+userInstance.id+'&redirect=http://localhost:8888',
+      verifyHref: 'https://rubric-expert.herokuapp.com/api/Accounts/confirm?uid='+userInstance.id+'&redirect=http://localhost:8888',
       user: Account,
       text: '{href}'
 
